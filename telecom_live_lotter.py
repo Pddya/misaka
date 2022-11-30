d212bd25801aeb6aef8ad00858fb039a26f987c4
Pddya <89203958+Pddya@users.noreply.github.com>
2022-11-30 01:38:27 +0000

    Update telecom_live_lotter.py

--- telecom_live_lotter.py
+++ telecom_live_lotter.py
@@ -131,8 +131,8 @@ class TelecomLotter:
             active_code2 = self.get_action_id_other(liveId)
             if active_code1 is not None or active_code2 is not None:
                 break
-            print(f"此直播间暂无抽奖活动, 等待2秒后再次查询 剩余查询次数{7 - i}")
-            await sleep(2)
+            print(f"此直播间暂无抽奖活动, 等待10秒后再次查询 剩余查询次数{7 - i}")
+            await sleep(10)
             continue
         if active_code1 is None and active_code2 is None:
             print("查询结束 本直播间暂无抽奖活动")
